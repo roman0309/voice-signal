512e9d479bed67a0f4b57688b1851b938d2dc70b
roman0309 <58557321+roman0309@users.noreply.github.com>
2026-03-06 16:24:32 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const WebSocket = require("ws")
 
-const wss = new WebSocket.Server({ port: 10000 })
+const port = process.env.PORT || 10000
+const wss = new WebSocket.Server({ port })
 
 let rooms = {}
 
@@ -12,32 +13,32 @@ ws.on("message", msg => {
 
 const data = JSON.parse(msg)
 
-if(data.type === "join"){
+if(data.type==="join"){
 
-room = data.room
+room=data.room
 
-if(!rooms[room]) rooms[room] = []
+if(!rooms[room]) rooms[room]=[]
 
 rooms[room].push(ws)
 
-if(rooms[room].length === 2){
+if(rooms[room].length===2){
 rooms[room][0].send(JSON.stringify({type:"createOffer"}))
 }
 
 return
 }
 
-rooms[room].forEach(client=>{
-if(client !== ws){
+rooms[room]?.forEach(client=>{
+if(client!==ws){
 client.send(msg.toString())
 }
 })
 
 })
 
-ws.on("close", ()=>{
+ws.on("close",()=>{
 if(room && rooms[room]){
-rooms[room] = rooms[room].filter(c=>c!==ws)
+rooms[room]=rooms[room].filter(c=>c!==ws)
 }
 })
 
